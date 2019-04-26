0b20cacce49b898811ee4d48a51cc579c1f5f2e5
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-26 01:50:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,13 @@
 # require libraries/modules here
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+
+  projects = kickstarter.css(".project_card")
+  binding.pry
 end
