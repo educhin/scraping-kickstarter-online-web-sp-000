42193afbe0e6e062d3ca5994cce66927f07a51c2
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-26 02:07:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = kickstarter.css(".project-card")
+  #title: project.css("h2.bbcard_name strong a").text
+  #pic: project.css("div.project-thumbnail a img").attribute("src").value
+  #description project.css("p.bbcard_blurb").text
+  #location project.css("ul.project-meta li a span.location-name").text
   binding.pry
 end
 
