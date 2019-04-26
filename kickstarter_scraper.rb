38835cc0324338c778c7a44ef9922ddeae6efbf6
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-26 02:09:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,6 +13,7 @@ def create_project_hash
   #pic: project.css("div.project-thumbnail a img").attribute("src").value
   #description project.css("p.bbcard_blurb").text
   #location project.css("ul.project-meta li a span.location-name").text
+  #precent funded: project.css("ul.project-stats li.first-funded strong").text
   binding.pry
 end
 
