6ae20a16fcdc2818e1929da29410842b62ac4c0c
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-26 02:15:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,19 +2,26 @@
 require 'nokogiri'
 require 'pry'
 
+#title: project.css("h2.bbcard_name strong a").text
+#pic: project.css("div.project-thumbnail a img").attribute("src").value
+#description project.css("p.bbcard_blurb").text
+#location project.css("ul.project-meta li a span.location-name").text
+#precent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
+
+
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
 
-  projects = kickstarter.css(".project-card")
-  #title: project.css("h2.bbcard_name strong a").text
-  #pic: project.css("div.project-thumbnail a img").attribute("src").value
-  #description project.css("p.bbcard_blurb").text
-  #location project.css("ul.project-meta li a span.location-name").text
-  #precent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-  binding.pry
+  projects = {}
+  
+  kickstarter.css(".project-card").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+  projects
 end
 
 create_project_hash
